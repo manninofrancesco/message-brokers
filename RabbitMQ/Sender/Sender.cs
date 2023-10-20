60bfc6368e42fc0f8e33167e8e47e89a9fef2b83
Francesco Mannino <francesco.mannino@yourbiz.it>
2023-10-20 16:04:14 +0200

    Add basic RabbitMQ communication

---
+++ RabbitMQ/Sender/Sender.cs
@@ -0,0 +1,32 @@
+﻿//https://github.com/rabbitmq/rabbitmq-tutorials/blob/main/dotnet/Send/Send.cs
+
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "queue",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine($"Write a message to send");
+    string? message = Console.ReadLine();
+
+    if (!string.IsNullOrWhiteSpace(message))
+    {
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: string.Empty,
+                             routingKey: "queue",
+                             basicProperties: null,
+                             body: body);
+    }
+}
